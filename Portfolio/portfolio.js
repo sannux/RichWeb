a8adb17354663d936f185edff0ab24a7608b4bd6
Sanita Tifentale <sanita.tifentale@mydit.ie>
2014-10-28 23:27:04 +0000

    update

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -1,26 +1,16 @@
 /**
  * 
  */
-function()
+var selectedNav = null; // keep track of currently selected
+
+function selectNav(el) 
 {
-	window.scroll
-	(
-		function()
-		{
-			var pos = window.scrollTop();
-			  
-			  if(pos<$("#services").offset().top - 91){
-				  select_nav("start");
-			  }else if(pos>($("#services").offset().top - 91) && pos<($("#services").offset().top + $("#services").height()) ){
-				  select_nav("services");
-			  }else if(pos>($("#work").offset().top - 91) && pos<($("#work").offset().top + $("#work").height() + 90) ){
-				  select_nav("work");
-			  }else if( pos>($("#clients").offset().top - 91) && pos<($("#clients").offset().top + $("#clients").height() + 90) ){
-				  select_nav("clients");
-			  }else if( pos>($("#book").offset().top - 91) && pos<($("#book").offset().top + $("#book").height() + 90) ){
-				  select_nav("book");
-			  }else{
-				  select_nav("contact");
-		}
-	)
+    if (selectedNav) // unselect selected
+    { 
+    	selectedNav.style.color = 'white';
+    	selectedNav.style.textShadow = 'none';
+    }
+    selectedNav = el; // new selected
+    el.style.textShadow = '0 0 5px #FFFFFF';
+    el.style.color = '#2e2e2e';
 }
